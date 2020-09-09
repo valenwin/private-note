1118382f3794517b02dd5e6a4852135c88f1390a
valenwin <tina.lysenok@gmail.com>
2020-09-09 18:44:20 +0300

    created Message model

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from ..core.models import Message
 
-# Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_filter = ('is_viewed',)
+    search_fields = ('text', 'is_viewed')
+    list_per_page = 20
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,46 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    text = models.TextField(
+        default=None,
+        null=True,
+        blank=True
+    )
+    access_token = models.CharField(
+        max_length=255,
+        default=None,
+        null=True,
+        blank=True
+    )
+    is_viewed = models.BooleanField(
+        default=None,
+        null=True,
+        blank=True
+    )
+    is_empty = models.BooleanField(
+        default=None,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f'ID:{self.id}, viewed: {self.is_viewed}'
+
+    def save(self, *args, **kwargs):
+        if not self.access_token:
+            self.access_token = f'{uuid.uuid1().hex}:{uuid.uuid4().hex}'
+
+        if not self.text:
+            self.is_empty = True
+
+        if self.is_viewed:
+            return self.delete()
+
+        return super(Message, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'message'
+        verbose_name_plural = 'messages'
